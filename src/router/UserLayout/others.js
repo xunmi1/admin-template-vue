330a1e34d67910a4ca613ca76dc58083041861d0
xunmi <xunmi1@outlook.com>
2019-04-30 20:04:39 +0800

    fix(router): 修复 login 组件嵌套

--- src/router/UserLayout/others.js
+++ src/router/UserLayout/others.js
@@ -2,25 +2,13 @@ import config from '@/config';
 
 export default [
     {
-        path: '/user',
-        name: 'user',
-        redirect: { name: config.loginName },
+        path: '/login',
+        name: config.loginName,
         meta: {
+            title: '用户登录',
             notAuth: true,
             notCache: true
         },
-        component: () => import(/* webpackChunkName: "UserLayout" */ '@/views/Layout/UserLayout'),
-        children: [
-            {
-                path: '/login',
-                name: config.loginName,
-                meta: {
-                    title: '用户登录',
-                    notAuth: true,
-                    notCache: true
-                },
-                component: () => import(/* webpackChunkName: "Login" */ '@/views/User/Login')
-            }
-        ]
+        component: () => import(/* webpackChunkName: "Login" */ '@/views/User/Login')
     }
 ];
