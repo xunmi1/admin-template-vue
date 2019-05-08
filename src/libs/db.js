7412e7d85f176dc8a54d231d440b734191914c27
xunmi <xunmi1@outlook.com>
2019-05-08 18:17:03 +0800

    refactor: use LightStorage

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,18 +1,6 @@
-import ProxyStorage from './common/ProxyStorage';
+import LightStorage from 'light-storage';
 import config from '@/config';
 
-// 扩展 ProxyStorage 类，增加静态方法(获取单例)
-class MyStorage extends ProxyStorage {
-    static getSingle (prefix) {
-        this.single = this.single || new Map();
-        if (!this.single.has(prefix)) {
-            this.single.set(prefix, new MyStorage(prefix));
-        }
-        return this.single.get(prefix);
-    }
-}
-
 // 全局使用同一 ProxyStorage 实例
-const db = MyStorage.getSingle(config.dbPrefix);
-
-export { db as default, MyStorage };
+const db = new LightStorage(config.dbPrefix);
+export default db;
