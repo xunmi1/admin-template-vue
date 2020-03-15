16d3a1416f44cc0d259d225bcc40051085361230
xunmi <xunmi1@outlook.com>
2020-03-15 11:24:16 +0800

    refactor: refactor router verify

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import NProgress from 'nprogress';
-import config from '../config';
-import db from '../libs/db';
-import store from '../store';
+import config from '@/config';
+import db from '@/libs/db';
+import store from '@/store';
 
-Vue.use(Router);
-
+Vue.use(VueRouter);
 /**
  * 加载路由
  * @param files 文件, 使用 `require.context()` 获取
@@ -15,26 +14,21 @@ Vue.use(Router);
 const loadRoutes = files =>
   files
     .keys()
-    .reduce((arr, key) => {
-      if (typeof files(key).default === 'object') {
-        return arr.concat(files(key).default);
-      }
-      return arr;
-    }, [])
+    .reduce((arr, key) => (typeof files(key).default === 'object' ? arr.concat(files(key).default) : arr), [])
     .sort((prev, next) => (prev.sort || 0) - (next.sort || 0));
 
 /**
  * path: string               仅第一级路径 path 前面加 '/'
  * meta: {
  *    title: string           显示在侧边栏、面包屑和标签栏的文字
- *    hideInBread: (false)    true: 此级路由将不会出现在面包屑中
- *    hideInMenu: (false)     true: 在菜单不会显示该页面选项(预留), **及其嵌套的子路由页面**
+ *    hideInBread: (false)    true: 此级路由将不会出现在面包屑中(预留)
+ *    hideInMenu: (false)     true: 在菜单不会显示该页面选项, **及其嵌套的子路由页面**
  *    notCache: (false)       true: 页面在切换标签后, **及其嵌套的子路由页面不会缓存**
  *    notAuth: (false)        true: 页面不需要验权，**若存在父级路由，所有父级路由也需设置 true，才能生效**
  *    icon: string            该页面在菜单、面包屑和标签导航处显示的图标
  * }
  */
-const router = new Router({
+const router = new VueRouter({
   routes: [
     {
       path: '/',
@@ -58,13 +52,26 @@ const router = new Router({
   ],
 });
 
-router.beforeEach(async (to, from, next) => {
+// 系统全局权限路由检查 如果前往路由的路由记录上有一个设置了验权, 则进行 token 校验
+// 类似于 'login' 等不验权的页面，需要其路由记录上，全部为 notAuth = true 时, 才会生效
+const checkRouterAuth = to => !to.matched.every(r => r.meta.notAuth);
+
+router.beforeEach((_, __, next) => {
   NProgress.start();
+  next();
+});
+
+router.beforeEach(async (to, from, next) => {
   if (checkRouterAuth(to)) {
-    if (!checkFirstEnter(next)) return;
-    if (!checkTokenValid(next)) return;
+    const stateToken = store.state.user.token;
+    if (!stateToken) {
+      const localToken = db.get('token');
+      if (!localToken) return navigateToLogin(next);
+      const remember = db.get('remember');
+      if (!remember) return navigateToLogin(next);
+      await initStoreData(localToken, remember);
+    }
   }
-  await initSystem();
   next();
 });
 
@@ -79,35 +86,9 @@ router.afterEach(to => {
 
 export default router;
 
-// 如果前往路由的路由记录上有一个设置了验权, 则进行 token 校验
-// 类似于 'login' 等不验权的页面，需要其路由记录上，全部为 notAuth = true 时, 才会生效
-const checkRouterAuth = function(to) {
-  return !to.matched.every(r => r.meta.notAuth);
-};
-
-const checkFirstEnter = function(next) {
-  if (store.state.user.token) return true;
-  const remember = db.get('remember');
-  if (!remember) {
-    next({ name: config.loginName });
-  }
-  return remember;
-};
-
-const checkTokenValid = function(next) {
-  const token = db.get('token');
-  if (!token) {
-    store.commit('user/setToken');
-    next({ name: config.loginName });
-  }
-  return !!token;
-};
-
-const initSystem = async function() {
-  if (store.state.user.token) return;
-  const token = db.get('token');
-  if (!token) return;
-  store.commit('user/setToken', { token, remember: true });
+// 初始化 `store` 信息
+const initStoreData = async (token, remember) => {
+  store.commit('user/setToken', { token, remember });
   store.commit('user/setUserInfo', db.get('userInfo'));
 };
 
@@ -128,4 +109,11 @@ const addAlive = function(to) {
       }
     }
   }
+};
+
+export const navigateToLogin = function(handler) {
+  // 清空 token 数据
+  store.commit('user/setToken');
+  if (handler) handler({ name: config.loginName });
+  else router.push({ name: config.loginName });
 };
