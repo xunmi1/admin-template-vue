542dd5853db44703b1cededdd7e4aecad48f3bf4
xunmi <xunmi1@outlook.com>
2020-04-04 18:16:35 +0800

    feat: add db key verification, and use constants

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+/**
+ * 保存应用的静态常量、枚举等
+ */
+
+/** 本地存储中的 key */
+export const StorageKeys = {
+  TOKEN: 'token',
+  USER_INFO: 'userInfo',
+  LOGIN_REMEMBER: 'login-remember',
+  BASIC_LAYOUT: 'basic-layout',
+};
--- src/libs/db.js
+++ src/libs/db.js
@@ -1,6 +1,38 @@
 import LightStorage from 'light-storage';
 import config from '@/config';
+import { StorageKeys } from '@/constants';
 
-// 全局使用同一 ProxyStorage 实例
-const db = new LightStorage(config.dbPrefix);
+// 将 StorageKeys 同时导出，便于使用
+export { StorageKeys };
+
+/**
+ * 劫持 LightStorage 实例，验证 key 值是否属于 StorageKeys
+ * @param {LightStorage} instance
+ * @return {LightStorage}
+ */
+const proxyStorage = instance => {
+  const collectionKeys = Object.values(StorageKeys);
+
+  const handler = {
+    apply(target, context, rest) {
+      const key = rest[0];
+      if (!collectionKeys.includes(key)) {
+        // eslint-disable-next-line
+        console.error(`unsafe storage key: ${key}, and key must be in the StorageKeys`);
+      }
+      return target.apply(context, rest);
+    },
+  };
+
+  instance.set = new Proxy(instance.set, handler);
+  instance.get = new Proxy(instance.get, handler);
+
+  return instance;
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const lightStorage = new LightStorage(config.dbPrefix);
+const db = isProduction ? lightStorage : proxyStorage(lightStorage);
+
 export default db;
