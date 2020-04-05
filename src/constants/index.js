37273d79a6dbf64e26c7903122824992b03a106b
xunmi <xunmi1@outlook.com>
2020-04-05 18:23:59 +0800

    fix: fix typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@
 /** 本地存储中的 key */
 export const StorageKeys = {
   TOKEN: 'token',
-  USER_INFO: 'userInfo',
+  USER_INFO: 'user-info',
   LOGIN_REMEMBER: 'login-remember',
   BASIC_LAYOUT: 'basic-layout',
 };
