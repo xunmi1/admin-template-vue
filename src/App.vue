23861933b07c1cd5107fe648fb0aa09f4e718c21
xunmi <xunmi1@outlook.com>
2020-05-22 20:43:40 +0800

    fix: replace provider

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <ALocaleProvider :locale="locale">
+    <AConfigProvider :locale="locale">
       <RouterView />
-    </ALocaleProvider>
+    </AConfigProvider>
   </div>
 </template>
 
