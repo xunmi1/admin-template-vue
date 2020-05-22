1562f32d95d06ddfa9d842cfddb8de319b592b04
xunmi <xunmi1@outlook.com>
2020-05-22 21:08:58 +0800

    refactor: freeze constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,10 +2,12 @@
  * 保存应用的静态常量、枚举等
  */
 
+const freeze = Object.freeze;
+
 /** 本地存储中的 key */
-export const StorageKeys = {
+export const StorageKeys = freeze({
   TOKEN: 'token',
   USER_INFO: 'user-info',
   LOGIN_REMEMBER: 'login-remember',
   BASIC_LAYOUT: 'basic-layout',
-};
+});
