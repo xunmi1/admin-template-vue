bdc31d112a385ff9904e5cf857b417b4c8bc2632
xunmi <xunmi1@outlook.com>
2020-07-01 14:13:50 +0800

    refactor: adapt Vuex v3.5

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createLogger from 'vuex/dist/logger';
 
 Vue.use(Vuex);
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // directory: 说明需要检索的目录
 // useSubdirectories: 是否检索子目录
@@ -13,16 +14,13 @@ files.keys().forEach(key => {
   modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default;
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  plugins.push(
-    createLogger({
-      filter: mutation => mutation.type !== 'app/addAlive',
-    })
-  );
+if (!isProduction) {
+  const logger = Vuex.createLogger({ filter: ({ type }) => type !== 'app/addAlive' });
+  plugins.push(logger);
 }
 
 export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production',
+  strict: !isProduction,
   state: {},
   mutations: {},
   actions: {},
